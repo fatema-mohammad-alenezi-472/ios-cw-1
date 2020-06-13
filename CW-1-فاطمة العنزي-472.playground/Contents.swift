5c0e54466a79f13e4c0904028e6148583f9531c4
fatema-mohammad-alenezi-472 <66527004+fatema-mohammad-alenezi-472@users.noreply.github.com>
2020-06-13 20:00:32 +0300

    classwork done

--- CW-1-فاطمة العنزي-472.playground/Contents.swift
+++ CW-1-فاطمة العنزي-472.playground/Contents.swift
@@ -1,14 +1,36 @@
 
 
-var name = " فاطمة محمد العنزي"
+let name = " فاطمة محمد العنزي"
 
-let age = 14
+var age = 14
 
-let hobby1 = "swimming"
+var hobby1 = "swimming"
 
-let hobby2 = "reading"
+var hobby2 = "reading"
 
-let height = 159
+var height = 159
 print("hello my name is",name, "i am ",age,"my hobbies are",hobby1,"and", hobby2 ,"my height is", height, "cm")
 
+
+
+
+let friendname = "ahood"
+
+var friendage = 14
+
+var friendheight = 155
+
+var friendhobby1 = "drawing"
+
+var friendhobby2 = "reading"
+
+var age_difference = age - friendage
+
+height > friendheight
+
+friendname >= name
+
+friendhobby1 != hobby1
+
+friendhobby2 <= hobby2
 
