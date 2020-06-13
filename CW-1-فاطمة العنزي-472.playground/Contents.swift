316f519f8b2d6f9f08db4cdbe5744f9de9f9452d
fatema-mohammad-alenezi-472 <66527004+fatema-mohammad-alenezi-472@users.noreply.github.com>
2020-06-13 18:59:43 +0300

    classwork one done.
    :)

---
+++ CW-1-فاطمة العنزي-472.playground/Contents.swift
@@ -0,0 +1,14 @@
+
+
+var name = " فاطمة محمد العنزي"
+
+let age = 14
+
+let hobby1 = "swimming"
+
+let hobby2 = "reading"
+
+let height = 159
+print("hello my name is",name, "i am ",age,"my hobbies are",hobby1,"and", hobby2 ,"my height is", height, "cm")
+
+
